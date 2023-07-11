64080490e855652808c2e00880e10f37ee33c4be
TV#BJJ <tv041000@gmail.com>
2023-07-11 10:35:03 +1000

    .

---
+++ src/main/java/com/example/coursework4/Coursework4Application.java
@@ -0,0 +1,13 @@
+package com.example.coursework4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coursework4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coursework4/Coursework4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursework4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coursework4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
